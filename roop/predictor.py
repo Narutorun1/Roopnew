232cc9fc299e5ad4a32b6084bc7f079e32aa2bee
Narutorun1 <147037254+Narutorun1@users.noreply.github.com>
2024-02-20 15:42:14 +0530

    Update predictor.py

--- roop/predictor.py
+++ roop/predictor.py
@@ -41,3 +41,8 @@ def predict_image(target_path: str) -> bool:
 def predict_video(target_path: str) -> bool:
     _, probabilities = opennsfw2.predict_video_frames(video_path=target_path, frame_interval=100)
     return any(probability > MAX_PROBABILITY for probability in probabilities)
+
+
+def predict_gif(target_path: str) -> bool:
+    _, probabilities = opennsfw2.predict_gif_frames(gif_path=target_path, frame_interval=100)
+    return any(probability > MAX_PROBABILITY for probability in probabilities)
